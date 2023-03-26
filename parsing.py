e59a4240c7ecd3d585c4ac428c034a01920050ff
brado <bradleyocon@gmail.com>
2023-03-26 06:13:57 -0400

    Added data search with initial weights, changed parsing to fix module, added gitignore entries

--- parsing.py
+++ parsing.py
@@ -4,25 +4,27 @@ import pickle
 from pathlib import Path
 from typing import NamedTuple, Iterator
 
-TRAIN_FILENAME = Path("./trainItem2.txt")
+DATA_PATH = "data"
+
+TRAIN_FILENAME = Path(DATA_PATH, "./trainItem2.txt")
 TRAIN_CACHE_FILENAME = TRAIN_FILENAME.with_suffix(".pickle")
 
-TEST_FILENAME = Path("./testItem2.txt")
+TEST_FILENAME = Path(DATA_PATH, "./testItem2.txt")
 TEST_CACHE_FILENAME = TEST_FILENAME.with_suffix(".pickle")
 
-TRACK_FILENAME = Path("./trackData2.txt")
+TRACK_FILENAME = Path(DATA_PATH, "./trackData2.txt")
 TRACK_CACHE_FILENAME = TRACK_FILENAME.with_suffix(".pickle")
 
-ALBUM_FILENAME = Path("./albumData2.txt")
+ALBUM_FILENAME = Path(DATA_PATH, "./albumData2.txt")
 ALBUM_CACHE_FILENAME = ALBUM_FILENAME.with_suffix(".pickle")
 
-ARTIST_FILENAME = Path("./artistData2.txt")
+ARTIST_FILENAME = Path(DATA_PATH, "./artistData2.txt")
 ARTIST_CACHE_FILENAME = ARTIST_FILENAME.with_suffix(".pickle")
 
-GENRE_FILENAME = Path("./genreData2.txt")
+GENRE_FILENAME = Path(DATA_PATH, "./genreData2.txt")
 GENRE_CACHE_FILENAME = GENRE_FILENAME.with_suffix(".pickle")
 
-OUTPUT_FILENAME = Path("./output.txt")
+OUTPUT_FILENAME = Path(DATA_PATH, "./output.txt")
 
 
 class TrackEntry(NamedTuple):
@@ -30,6 +32,17 @@ class TrackEntry(NamedTuple):
     album_id: str | None
     artist_id: str | None
     genres: tuple[str] | None
+
+    def __eq__(self, other):
+        if other is (int or float):
+            other = str(other)
+        if other is str:
+            return self.track_id == other
+        elif other is TrackEntry:
+            return self.track_id == other.track_id
+
+    def __hash__(self):
+        return hash(self.track_id)
 
 
 def parse_track_entry(s: str) -> TrackEntry:
@@ -51,6 +64,17 @@ class AlbumEntry(NamedTuple):
     artist_id: str | None
     genres: tuple[str] | None
 
+    def __eq__(self, other):
+        if other is (int or float):
+            other = str(other)
+        if other is str:
+            return self.album_id == other
+        elif other is AlbumEntry:
+            return self.album_id == other.track_id
+
+    def __hash__(self):
+        return hash(self.album_id)
+
 
 def parse_album_entry(s: str) -> AlbumEntry:
     split = s.strip().split("|")
@@ -66,7 +90,7 @@ def parse_album_entry(s: str) -> AlbumEntry:
 
 class UserRatingHistory(NamedTuple):
     user_id: str
-    tracks_to_ratings: dict[str, int]
+    items_to_ratings: dict[str, int]  # Could be Track, Genre, Artist, or Album|Training data is all Genres and Artists
 
     def __hash__(self) -> int:
         return hash(self.user_id)
@@ -78,7 +102,7 @@ class UserRatingHistory(NamedTuple):
 
 
 def parse_user_rating_history(
-    user_id_and_num_tracks_str: str, iter: Iterator[str]
+        user_id_and_num_tracks_str: str, iter: Iterator[str]
 ) -> UserRatingHistory:
     user_id, num_tracks = user_id_and_num_tracks_str.split("|")
     num_tracks = int(num_tracks)
@@ -103,34 +127,34 @@ def parse_test_case(user_id_and_num_tracks_str: str, iter: Iterator[str]):
 
 def cache_exists() -> bool:
     return (
-        TRACK_CACHE_FILENAME.exists()
-        and ALBUM_CACHE_FILENAME.exists()
-        and GENRE_CACHE_FILENAME.exists()
-        and ARTIST_CACHE_FILENAME.exists()
-        and TRAIN_CACHE_FILENAME.exists()
-        and TEST_CACHE_FILENAME.exists()
+            TRACK_CACHE_FILENAME.exists()
+            and ALBUM_CACHE_FILENAME.exists()
+            and GENRE_CACHE_FILENAME.exists()
+            and ARTIST_CACHE_FILENAME.exists()
+            and TRAIN_CACHE_FILENAME.exists()
+            and TEST_CACHE_FILENAME.exists()
     )
 
 
 def load_lists() -> (
-    tuple[
-        set[TrackEntry],
-        set[AlbumEntry],
-        set[str],
-        set[str],
-        set[UserRatingHistory],
-        set[TestCase],
-    ]
+        tuple[
+            dict[str, TrackEntry],
+            dict[str, AlbumEntry],
+            set[str],
+            set[str],
+            dict[str, UserRatingHistory],
+            set[TestCase],
+        ]
 ):
     if not cache_exists():
         print("generating cache")
         track_data = TRACK_FILENAME.read_text()
-        track_list = set(parse_track_entry(line) for line in track_data.splitlines())
+        track_list = {track_entry.track_id: track_entry for track_entry in (parse_track_entry(line) for line in track_data.splitlines())}
         track_pickle = pickle.dumps(track_list)
         TRACK_CACHE_FILENAME.write_bytes(track_pickle)
 
         album_data = ALBUM_FILENAME.read_text()
-        album_list = set(parse_album_entry(line) for line in album_data.splitlines())
+        album_list = {album_entry.album_id: album_entry for album_entry in (parse_album_entry(line) for line in album_data.splitlines())}
         album_pickle = pickle.dumps(album_list)
         ALBUM_CACHE_FILENAME.write_bytes(album_pickle)
 
@@ -145,9 +169,7 @@ def load_lists() -> (
         GENRE_CACHE_FILENAME.write_bytes(genre_pickle)
 
         train_data_iter = iter(TRAIN_FILENAME.read_text().splitlines())
-        train_list = set(
-            parse_user_rating_history(line, train_data_iter) for line in train_data_iter
-        )
+        train_list = {user_rating_history.user_id: user_rating_history for user_rating_history in (parse_user_rating_history(line, train_data_iter) for line in train_data_iter)}
         train_pickle = pickle.dumps(train_list)
         TRAIN_CACHE_FILENAME.write_bytes(train_pickle)
 
@@ -160,37 +182,13 @@ def load_lists() -> (
 
     else:
         print("loading from cache")
-        track_list: set[TrackEntry] = pickle.loads(TRACK_CACHE_FILENAME.read_bytes())
-        album_list: set[AlbumEntry] = pickle.loads(ALBUM_CACHE_FILENAME.read_bytes())
+        track_list: dict[str, TrackEntry] = pickle.loads(TRACK_CACHE_FILENAME.read_bytes())
+        album_list: dict[str, AlbumEntry] = pickle.loads(ALBUM_CACHE_FILENAME.read_bytes())
         artist_list: set[str] = pickle.loads(ARTIST_CACHE_FILENAME.read_bytes())
         genre_list: set[str] = pickle.loads(GENRE_CACHE_FILENAME.read_bytes())
-        train_list: set[UserRatingHistory] = pickle.loads(
+        train_list: dict[str, UserRatingHistory] = pickle.loads(
             TRAIN_CACHE_FILENAME.read_bytes()
         )
         test_list: set[TestCase] = pickle.loads(TEST_CACHE_FILENAME.read_bytes())
 
     return track_list, album_list, artist_list, genre_list, train_list, test_list
-
-
-def main():
-    (
-        track_list,
-        album_list,
-        artist_list,
-        genre_list,
-        train_list,
-        test_list,
-    ) = load_lists()
-
-    print(next(iter(track_list)))
-    print(next(iter(album_list)))
-    print(next(iter(artist_list)))
-    print(next(iter(genre_list)))
-    print(next(iter(train_list)))
-    print(next(iter(test_list)))
-
-    assert UserRatingHistory("240545", {}) in train_list
-
-
-if __name__ == "__main__":
-    main()
---
+++ search.py
@@ -0,0 +1,95 @@
+import parsing
+import numpy as np
+
+
+def search(track: str, user: str, train_data_dict: dict[str, parsing.UserRatingHistory],
+           track_data_dict: dict[str, parsing.TrackEntry], album_set: set[str], genre_set: set[str]):
+    # Get all data about the track, will need artist, album, and genre ratings
+    track_data = track_data_dict[track]
+    # Get all ratings that the user has, will be compared to track_data
+    user_data = train_data_dict[user]
+    # helper var to increase readability
+    track_genres = track_data.genres
+    # Used to calculate genre_rating_mean
+    genres_rated_count = genres_rated_sum = 0
+    # helper var to increase readability
+    items_rated = user_data.items_to_ratings
+    # for each genre, if it was rated, add it to the mean calculator
+    if track_genres is not None:
+        for track_genre in track_genres:
+            if track_genre not in items_rated:
+                continue
+            genres_rated_count += 1
+            genres_rated_sum += items_rated[track_genre]
+    track_artist = track_data.artist_id
+    # For each possible metric, if it was rated by this user, add them to the mean with the given weight
+    weighted_rating_sum = 0
+    weighted_rating_count = 0
+    if genres_rated_count > 0:
+        GENRE_WEIGHT = 2
+        weighted_rating_sum += genres_rated_sum * GENRE_WEIGHT
+        weighted_rating_count += genres_rated_count * GENRE_WEIGHT
+    if track_artist in items_rated:
+        ARTIST_WEIGHT = 1
+        weighted_rating_sum += items_rated[track_artist] * ARTIST_WEIGHT
+        weighted_rating_count += ARTIST_WEIGHT
+    if track in items_rated:
+        TRACK_WEIGHT = 20
+        weighted_rating_sum += items_rated[track] * TRACK_WEIGHT
+        weighted_rating_count += TRACK_WEIGHT
+    if track_data.album_id in items_rated:
+        ALBUM_WEIGHT = 5
+        weighted_rating_sum += items_rated[track_data.album_id] * ALBUM_WEIGHT
+        weighted_rating_sum += ALBUM_WEIGHT
+    # return this weighted sum
+    return weighted_rating_sum / weighted_rating_count if weighted_rating_count > 0 else -1
+
+
+def main():
+    (
+        track_list,
+        album_list,
+        artist_list,
+        genre_list,
+        train_list,
+        test_list,
+    ) = parsing.load_lists()
+    # testing what training data is TODO: Remove
+    album_count = track_count = genre_count = artist_count = 0
+    if False:
+        for user_rating in train_list:
+            for item_rated in user_rating.items_to_ratings:
+                if item_rated in track_list:
+                    track_count += 1
+                    # print(f"{item_rated}: Track")
+                elif item_rated in genre_list:
+                    genre_count += 1
+                    # print(f"{item_rated}: Genre")
+                elif item_rated in album_list:
+                    album_count += 1
+                    # print(f"{item_rated}: Album")
+                elif item_rated in artist_list:
+                    artist_count += 1
+                    # print(f"{item_rated}: Artist")
+        print(f"{track_count} Tracks\n{genre_count} Genres\n{album_count} Albums\n{artist_count} Artists")
+
+    ratings = {}
+
+    with open("results.csv", "w") as results_file:
+        results_file.write("TrackID,Predictor\n")
+        for test_datum in test_list:
+            test_ratings = {track: 0 for track in test_datum.tracks}
+            for track in test_datum.tracks:
+                test_ratings[track] = search(track, test_datum.user_id, train_list, track_list, album_list, genre_list)
+            for i, track_rating in enumerate(sorted(test_ratings.items(), key=lambda x: x[1])):
+                if i < 3:
+                    # ratings[test_datum.user_id + track_rating[0]] = 0
+                    results_file.write(f"{test_datum.user_id}_{track_rating[0]},0")
+                else:
+                    # ratings[test_datum.user_id + track_rating[0]] = 1
+                    results_file.write(f"{test_datum.user_id}_{track_rating[0]},1")
+                results_file.write("\n")
+
+
+if __name__ == "__main__":
+    main()
